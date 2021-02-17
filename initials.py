3bfe1a2cb4d711ad64fdce1ddc7e075f632e5f4e
jakub Fras <72709789+Jf-dev1@users.noreply.github.com>
2021-02-17 17:07:31 +0000

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,9 @@
+#jakub fras
+# i like IT
+print('      JJJJJ       FFFFF  ')
+print('        J         F      ')
+print('        J         FFF     ')
+print('        J         F      ')
+print('  J     J         F      ')
+print('   J    J         F      ')
+print('    J  J          F      ') 
